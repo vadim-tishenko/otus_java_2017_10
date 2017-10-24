50ac880cc39c5d3e2c006d071bcf5db7f0e5b40d
tishenko <vad.tishenko@gmail.com>
2017-10-24 16:44:12 +0300

    start add reflection

--- hw02/stand/src/main/java/ru/cwl/otus/StandWithInstrumentation.java
+++ hw02/stand/src/main/java/ru/cwl/otus/StandWithInstrumentation.java
@@ -6,29 +6,68 @@ package ru.cwl.otus; /**
 
 import ru.cwl.hw02.agent.MemoryAgent;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class StandWithInstrumentation {
     public static void main(String[] args) {
-        printObjectSize(new Object());
+//        printObjectSize(new Object());
         printObjectSize(new A());
-        printObjectSize(1);
+       /* printObjectSize(1);
         printObjectSize("string");
         printObjectSize(Calendar.getInstance());
         printObjectSize(new BigDecimal("999999999999999.999"));
         printObjectSize(new ArrayList<String>());
-        printObjectSize(new Integer[100]);
+        printObjectSize(new Integer[100]);*/
+
     }
 
     public static void printObjectSize(Object obj) {
-        System.out.println(String.format("%s, size=%s", obj.getClass()
-                .getSimpleName(), MemoryAgent.getSize(obj)));
+
+
+        System.out.println(String.format("%s, size=%s", obj.getClass().getSimpleName(), MemoryAgent.getSize(obj)));
+
+        System.out.println("-- fields --");
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            // +не статичесткие
+            if (Modifier.isStatic(field.getModifiers())) {
+                continue;
+            }
+            System.out.println("-");
+            //field.getGenericType()
+            // если это ссылка != null, идем по ней. не забываем про зацыкливание.
+            final Class<?> type = field.getType();
+            boolean isArray = type.isArray();
+            boolean isObject = !type.isPrimitive();
+            String name = type.getName();
+
+
+            System.out.printf("name: %s, array: %s obj: %s\n", name, isArray, isObject);
+
+            System.out.printf("%s %s\n", field.getName(), field.getGenericType().getTypeName());
+            field.setAccessible(true);
+            try {
+                Object value = field.get(obj);
+                System.out.printf("v: %s o: %s\n", value, value instanceof Object);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("----");
+
     }
 }
 
 class A {
-    Integer id;
+    Integer id = 1000;
     String name;
+    long l;
+    long d;
+    int[] intArray = new int[100];
+    Integer[] intArray2 = new Integer[100];
+    List<String> lstString = new ArrayList<>();
 }
