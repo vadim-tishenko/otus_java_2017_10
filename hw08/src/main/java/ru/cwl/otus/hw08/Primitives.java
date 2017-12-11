c907d3d54ef73a46263d4e476b176ba3007e7ab0
Vadim Tishenko <vad.tishenko@gmail.com>
2017-12-11 23:12:37 +0300

    hw08 add test dependency

---
+++ hw08/src/main/java/ru/cwl/otus/hw08/Primitives.java
@@ -0,0 +1,13 @@
+package ru.cwl.otus.hw08;
+
+/**
+ * Created by vadim.tishenko
+ * on 11.12.2017 22:54.
+ */
+public class Primitives {
+    public int i;
+    public long l;
+    public double d;
+    public float f;
+
+}
