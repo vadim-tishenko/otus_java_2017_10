6d1b57f7afd11ca06ff31008f6fb7120e43380ea
tishenko <vad.tishenko@gmail.com>
2018-03-16 18:59:50 +0300

    hw16 add proc builder...

--- hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
+++ hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
@@ -1,10 +1,20 @@
 package ru.cwl.otus.hw16.loader;
 
+import java.io.IOException;
+
 /**
  * Created by tischenko on 16.03.2018 17:05.
  */
 public class AppLoader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("loader started");
+        ProcessBuilder pb=new ProcessBuilder("java","-jar","hw16/front/target/front-1.0.jar");
+        Process p = pb.start();
+
+        while (p.isAlive()){
+            Thread.sleep(1);
+            System.out.print(".");
+        }
+        System.out.println(p.exitValue());
     }
 }
