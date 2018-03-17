2025e862f30dcbab7c8c906fc242c15fe1239086
Vadim Tishenko <vad.tishenko@gmail.com>
2018-03-17 13:50:11 +0300

    hw16 apploader

--- hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
+++ hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
@@ -1,6 +1,9 @@
 package ru.cwl.otus.hw16.loader;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by tischenko on 16.03.2018 17:05.
@@ -8,8 +11,18 @@ import java.io.IOException;
 public class AppLoader {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("loader started");
-        ProcessBuilder pb=new ProcessBuilder("java","-jar","hw16/front/target/front-1.0.jar");
+        ProcessBuilder pb=new ProcessBuilder("java","-jar","hw16/front/target/front-1.0.jar","front-1");
         Process p = pb.start();
+
+        InputStream is = p.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+        System.out.println("---");
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        System.out.println("---");
 
         while (p.isAlive()){
             Thread.sleep(1);
