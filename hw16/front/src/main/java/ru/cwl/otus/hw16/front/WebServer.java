b32e14643c73e7e691c3072090869bae92b6fcaf
Vadim Tishenko <vad.tishenko@gmail.com>
2018-03-18 09:34:14 +0300

    hw16

--- hw16/front/src/main/java/ru/cwl/otus/hw16/front/WebServer.java
+++ hw16/front/src/main/java/ru/cwl/otus/hw16/front/WebServer.java
@@ -1,10 +1,23 @@
 package ru.cwl.otus.hw16.front;
 
+import java.util.Scanner;
+
 /**
  * Created by tischenko on 16.03.2018 17:08.
  */
 public class WebServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("web server started");
+        for (String arg : args) {
+            System.out.println("arg:" + arg);
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if ("q".equals(line)) break;
+            System.out.println("in:" + line);
+        }
+
+        // Thread.sleep(5000);
     }
 }
--- hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
+++ hw16/loader/src/main/java/ru/cwl/otus/hw16/loader/AppLoader.java
@@ -1,5 +1,8 @@
 package ru.cwl.otus.hw16.loader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,25 +12,28 @@ import java.io.InputStreamReader;
  * Created by tischenko on 16.03.2018 17:05.
  */
 public class AppLoader {
+    private static final Logger log = LoggerFactory.getLogger(AppLoader.class);
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("loader started");
-        ProcessBuilder pb=new ProcessBuilder("java","-jar","hw16/front/target/front-1.0.jar","front-1");
+        log.info("loader started");
+        ProcessBuilder pb = new ProcessBuilder("java", "-Daaa=bbb", "-jar", "hw16/front/target/front-1.0.jar", "front-1", "localhost", "2345");
         Process p = pb.start();
+        log.info("process {} started", p);
 
         InputStream is = p.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
+
         String line = null;
-        System.out.println("---");
+        log.info("---");
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            log.info("p: " + line);
         }
-        System.out.println("---");
+        log.info("---");
 
         while (p.isAlive()){
             Thread.sleep(1);
             System.out.print(".");
         }
-        System.out.println(p.exitValue());
+        log.info("exit value:{}", p.exitValue());
     }
 }
